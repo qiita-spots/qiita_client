197685beae95ef6877f7d5c7e6d523f802f67b5e
Charles Cowart <ccowart@BigMac.local>
2019-03-06 13:34:43 -0800

    Debugging level set by env variable
    Debugging level for qiita_client now set by the environment variable
    'QIITA_CLIENT_DEBUG_LEVEL'.

--- qiita_client/__init__.py
+++ qiita_client/__init__.py
@@ -13,6 +13,7 @@ from .plugin import (QiitaCommand, QiitaPlugin, QiitaTypePlugin,
                      QiitaArtifactType)
 
 import logging
+from os import environ
 
 handler = logging.StreamHandler()
 fmt_str = '%(asctime)s %(name)-12s %(levelname)-8s %(message)s'
@@ -20,13 +21,29 @@ handler.setFormatter(logging.Formatter(fmt_str))
 logger = logging.getLogger(__name__)
 logger.addHandler(handler)
 
+debug_levels_list = [('DEBUG', logging.DEBUG),
+                     ('INFO', logging.INFO),
+                     ('WARNING', logging.WARNING),
+                     ('ERROR', logging.ERROR),
+                     ('CRITICAL', logging.CRITICAL)]
+
 # logging level can be set to one of five levels:
 # logging.DEBUG     (lowest level, includes ALL messages)
 # logging.INFO      (next lowest level)
 # logging.WARNING   (self-explanatory)
 # logging.ERROR     (self-explanatory)
 # logging.CRITICAL  (self-explanatory)
-logger.setLevel(logging.DEBUG)
+if 'QIITA_CLIENT_DEBUG_LEVEL' in environ:
+    s = environ['QIITA_CLIENT_DEBUG_LEVEL']
+    m = [x for x in debug_levels_list if x[0] == s]
+    if m:
+        logger.setLevel(m[1])
+    else:
+        s = "%s is not a valid value for QIITA_CLIENT_DEBUG_LEVEL" % s
+        raise ValueError(s)
+else:
+    logger.setLevel(logging.CRITICAL)
+
 
 logger.debug('logging instantiated and configured')
 
