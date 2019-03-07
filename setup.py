ac995e6b32888498b2017726bd5c54e45abe0d76
Antonio Gonzalez <antgonza@gmail.com>
2019-03-07 05:05:41 -0700

    improve setup.py

--- setup.py
+++ setup.py
@@ -39,8 +39,6 @@ setup(name='qiita-client',
       url='https://github.com/biocore/qiita',
       test_suite='nose.collector',
       packages=['qiita_client'],
-      extras_require={'test': ["nose >= 0.10.1", "pep8"]},
-      install_requires=['click >= 3.3', 'requests', 'future',
-                        'pandas'],
+      install_requires=['click', 'requests', 'future', 'nose', 'pep8'],
       classifiers=classifiers
       )
