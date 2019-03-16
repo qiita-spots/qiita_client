515b36535ef9427e05a6ee5d6bd605a3aa40fbd1
Daniel McDonald <danielmcdonald@ucsd.edu>
2019-03-16 16:57:21 -0700

    Doesn't make sense...

--- qiita_client/qiita_client.py
+++ qiita_client/qiita_client.py
@@ -385,7 +385,8 @@ class QiitaClient(object):
             The JSON response from the server
         """
         logger.debug('Entered QiitaClient.post(%s)' % url)
-        return self._request_retry(self._session.post, url, **kwargs)
+        #return self._request_retry(self._session.post, url, **kwargs)
+        return self._request_retry(requests.post, url, **kwargs)
 
     def patch(self, url, **kwargs):
         """Executes a HTTP patch request against the Qiita server
