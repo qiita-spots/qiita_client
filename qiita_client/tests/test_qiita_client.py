04e0c4ade99801272a530e28ce7479cc4afbb20b
Antonio Gonzalez <antgonza@gmail.com>
2024-07-22 10:21:14 -0600

    build test 3

--- qiita_client/tests/test_qiita_client.py
+++ qiita_client/tests/test_qiita_client.py
@@ -236,7 +236,7 @@ class QiitaClientTests(PluginTestCase):
         # Create a new job
         data = {
             'user': 'demo@microbio.me',
-            'command': dumps(['QIIME', '1.9.1', 'Pick closed-reference OTUs']),
+            'command': dumps(['QIIMEq2', '1.9.1', 'Pick closed-reference OTUs']),
             'status': 'running',
             'parameters': dumps({"reference": 1,
                                  "sortmerna_e_value": 1,
