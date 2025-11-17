3f38b1306eeba491e6e900436edb28e2f9724dcf
Stefan Janssen <stefan.m.janssen@gmail.com>
2025-11-17 16:31:21 +0100

    codestyle

--- qiita_client/qiita_client.py
+++ qiita_client/qiita_client.py
@@ -581,7 +581,7 @@ class QiitaClient(object):
            (path == '/html_summary/') and (op == 'add'):
             if re.search(r"/qiita_db/artifacts/\d+/?$", url):
                 if value is not None:
-                    logger.debug('QiitaClient::patch: push summary files to' \
+                    logger.debug('QiitaClient::patch: push summary files to'
                                  'central: %s' % value)
                     try:
                         # values might be an json encoded dictioary with
@@ -591,7 +591,7 @@ class QiitaClient(object):
                             if (ftype in dictValues.keys()) and \
                                (dictValues[ftype] is not None):
                                 self.push_file_to_central(dictValues[ftype])
-                    except TypeError as e:
+                    except TypeError:
                         # or just a single string, i.e. filepath
                         self.push_file_to_central(value)
 
